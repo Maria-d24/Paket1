0d445e809ae747745f6b9b851b9adad0094c33bc
Maria <maria-d24@mail.ru>
2020-02-17 21:34:51 +0300

    HW4 test

--- src/Paket/HW4/HW4/Task1.java
+++ src/Paket/HW4/HW4/Task1.java
@@ -3,7 +3,6 @@ package Paket.HW4.HW4;
 public class Task1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.println("Hello,  Alex!");
+        
     }
 }
